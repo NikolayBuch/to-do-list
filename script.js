96bcc8212b04d69c7ade35dac45ad8f7fe11a555
Nikolay <NikolayBuch3@gmail.com>
2022-12-17 22:16:22 +0300

    edits2

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const form =  document.querySelector('.todo-list');
 const addMessege = document.querySelector('.messege');
-const addButton = document.querySelector('.add-btn');
 const tasksList = document.querySelector('.tasks-list');
 const filters = document.querySelectorAll('.filters span');
-const body = document.querySelector('.body');
 const clearChecked = document.querySelector('.tasks__clear');
 const todoAll = document.querySelector('.todo__all');
+const pending = document.querySelector('.pending')
     
 let todoList = [];
 let activeFilter = 'All'
@@ -22,26 +21,22 @@ const addTask = (e) => {
     renderTask(todoList)
     addMessege.value = '';
     addMessege.focus();
-
     seveTodoList()    
 };
+
 const renderTask = (list) => {
         const renderItem = list.map((item) =>`
              <li id='${item.id}' class='task'data-action="li" >
                  <div class='task__continer'>
                      <lable class='custom-checkbox'  >
-                         <input class='task__checked' data-action="done"  type='checkbox' id='${item.id}' ${item.checked ? 'checked' : ''}  ><p id=${item.id} class=${item.checked ? 'task__messeg task__messeg_checked' : 'task__messeg'}>${item.text}</p>
+                         <input class='task__checked' onclick="updateStatus(this)" data-action="done"  type='checkbox' id='${item.id}' ${item.checked ? 'checked' : ''}  >
+                         <p id=${item.id} class=${item.checked ? 'checked ' : ''}>${item.text}</p>
                      </lable>
                      <button data-action="delete" class='clouse-btn' id=''><img src='./img/close.svg' alt='close'> </button>
                  </div>
              </li>`)
          tasksList.innerHTML = renderItem.join('')
 };
-
-const counterTodo = () => {
-    const valCounterTodo = todoList.length;
-    console.loge(valCounterTodo)
-}
 
 const deleteTask = (e) => {
     if(e.target.dataset.action !== 'delete') return;
@@ -51,66 +46,79 @@ const deleteTask = (e) => {
 
         const index = todoList.findIndex((todo) => todo.id === idTask);
         const deleteId = todoList.splice(index, 1)   
-
         seveTodoList()
 };
 
-const doneTask = (e) => {
+const deleteChecked = (e) => {
+    if(e.target.dataset.action !== 'deleteChecked') return;
+    const checkedTask = todoList.filter(newTodo => newTodo.checked)
+    for (checkedTask; checkedTask > 0; checkedTask.remove()){
+        renderTask(checkedTask)
+    }
+    const deleteChecked = todoList.splice(checkedTask, 2)
+}
 
-    if(e.target.dataset.action !== 'done') return;
-        const parentNode = e.target.closest('.task');
-        const idTask = Number(parentNode.id)
+const showPending = () => {
+    const completedTasks = todoList.filter(newTodo => !newTodo.checked)
+    const renderPeding = `<p class="p-normal"> ${completedTasks.length} items left</p>`
+    pending.innerHTML = renderPeding
+}
 
-        const task = todoList.find((task) => {
-            if (task.id == idTask){
-                return true;
-            }
-        });
-        task.checked = !task.checked;
-        const taskTatle = parentNode.querySelector('.task__messeg').classList.toggle('task__messeg_checked');
-        const showClearChecked = todoList.filter(newTodo => newTodo.checked);
-        if (showClearChecked == 0) {
-            clearChecked.classList.add('none')
-        } else {
-            clearChecked.classList.remove('none')
+const showClearChecked = () =>{
+    const checkedTask= todoList.filter(newTodo => newTodo.checked);
+    if (checkedTask == 0) {
+        clearChecked.classList.add('none')
+    } else {
+        clearChecked.classList.remove('none')
+    }
+}
+
+const updateStatus = (selectedTask) => {
+    let taskName = selectedTask.parentNode.lastElementChild;
+    if(selectedTask.checked) {
+        taskName.classList.add('checked')
+    }else{
+        taskName.classList.remove('checked')
+    }
+    const task = todoList.find((task) => {
+        if(task.id == taskName.id){
+            return true
         }
-        seveTodoList()
-        
-  
-};
+    })
+    task.checked = !task.checked
+    showClearChecked()
+    seveTodoList()
+    showPending()
+}
 
-const renderFilterItems = (filter) => {
-    switch (filter) {
-        case 'all':
-            activeFilter = 'all';
-            renderTask(todoList);
-            break;
-        case 'completed':
-            activeFilter = 'completed';
-            const completedTasks = todoList.filter(newTodo => newTodo.checked);
-            renderTask(completedTasks);
-            break;
-        case 'pending':
-            activeFilter = 'pending';
-            const pendingTasks = todoList.filter(newTodo => !newTodo.checked);
-            renderTask(pendingTasks);
-            break;
-        default:
-            renderTask(todoList);
-    };
-};
+renderFilterItems = (filter) => {
+    if (filter =='all') {
+        activeFilter = 'all'
+        renderTask(todoList)
+    } else if (filter == 'completed') {
+        activeFilter = 'completed'
+        const completedTasks = todoList.filter(newTodo => {
+        return newTodo.checked})
+        renderTask(completedTasks)
+    } else if(filter == 'pending') {
+        activeFilter = 'pending'
+        const pendingTasks = todoList.filter(newTodo => {
+            return !newTodo.checked
+        })
+        renderTask(pendingTasks)
+    }
+
+}
 
 filters.forEach((filter) => filter.addEventListener('click', () => {
     document.querySelector("span.active").classList.remove("active");
     filter.classList.add("active");
-    renderFilterItems(filter.id)
-}
+    renderFilterItems(filter.id)}
 ));
 
 tasksList.addEventListener('click', deleteTask)
-tasksList.addEventListener('click', doneTask)
 form.addEventListener('submit', addTask);
-// body.addEventListener('click', addTask)
+clearChecked.addEventListener("click", deleteChecked)
 
 const seveTodoList = () => {
     localStorage.setItem('todoList', JSON.stringify(todoList));
@@ -118,4 +126,6 @@ const seveTodoList = () => {
 if(localStorage.getItem('todoList')) {
     todoList = JSON.parse(localStorage.getItem('todoList'));
     renderTask(todoList);
+    showClearChecked()
+    showPending()
 }
